a607fd85dc93a5f159ead11ce958bb5bc36acb7c
kiennthe150160 <kiennthe150160@fpt.edu.vn>
2021-08-27 13:55:45 +0700

    update

--- Dog.js
+++ Dog.js
@@ -1,9 +1,17 @@
-function Dog()
+var chalk = require("chalk");
+
+function Dog(name)
 {
+	this.name = name;
 	this.stomach = [];
 }
 
 Dog.prototype.eat = function(Cat) 
 {
 	this.stomach.push(Cat);
+};
+
+Dog.prototype.say = function() 
+{
+	console.log("Hi! I am " + chalk.blue(this.name));
 };
